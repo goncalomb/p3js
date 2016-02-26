4bad912385810005dc3d539806547cba7d3b6ef9
goncalomb <me@goncalomb.com>
2016-02-26 23:47:24 +0000

    Add drop-down menu to load demos

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,18 +1,32 @@
 $(window).ready(function() {
 
 	var $code = $("#code");
+	var $load_demo = $("#load-demo")
 	var $output = $("#output");
 	var $assemble = $("#assemble");
 	var $assemble_run = $("#assemble-run");
 	var $assemble_dl = $("#assemble-dl");
 
-	// Loading local files (file://) with jQuery may not work. Use:
-	// google-chrome --user-data-dir=$(mktemp -d) --incognito --allow-file-access-from-files --start-maximized
-	// Or use Firefox.
-	$.get("demos/Demo1-clean.as", null, function(data) {
-		$code.val(data);
-		$run.click();
-	}, "text");
+	var demos = [
+		// "welcome.as",
+		"Demo1-clean.as"
+	];
+	demos.forEach(function(demo) {
+		$load_demo.append($("<option>").val(demo).text(demo));
+	});
+	function load_demo(demo) {
+		// Loading local files (file://) with jQuery may not work. Use:
+		// google-chrome --user-data-dir=$(mktemp -d) --incognito --allow-file-access-from-files --start-maximized
+		// Or use Firefox.
+		$.get("demos/" + demo, null, function(data) {
+			$code.val(data);
+		}, "text");
+	}
+	$load_demo.change(function() {
+		var demo = $("option:selected", this).val();
+		load_demo(demo)
+	});
+	load_demo(demos[0]);
 
 	function try_assemble() {
 		try {

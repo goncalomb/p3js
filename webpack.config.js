d01b4437897cef256d97e4d46a255158dfa566db
goncalomb <me@goncalomb.com>
2016-03-20 17:30:42 +0000

    Start using webpack for all javascript code

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 var webpack = require("webpack");
 var path = require("path");
+var output_path = path.join(__dirname, "www", "scripts");
+var copyright = "Copyright (C) 2016 Gonçalo Baltazar <me@goncalomb.com>";
 
-module.exports = {
-	entry: [ "./src/p3js.js" ],
+module.exports = [{
+	entry: "./src/p3js/p3js.js",
 	output: {
-		path: path.join(__dirname, "www", "scripts"),
+		path: output_path,
 		filename: "p3js-bundle.js",
 	},
 	plugins: [
 		new webpack.BannerPlugin(
 			"P3JS Core Bundle\n" +
-			"Creates 'window.p3js'."
+			"P3 CPU Assembler and Simulator.\n" +
+			"Creates 'window.p3js'.\n" +
+			"\n" + copyright
 		)
 	]
-};
+}, {
+	entry: "./src/p3js-web/main.js",
+	output: {
+		path: output_path,
+		filename: "p3js-web-bundle.js",
+	},
+	plugins: [
+		new webpack.BannerPlugin(
+			"P3JS Web Bundle\n" +
+			"Web UI for P3JS.\n" +
+			"\n" + copyright
+		)
+	]
+}];

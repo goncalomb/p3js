d8a49357646a6e9ad25899faa9a9af58c2e23516
goncalomb <me@goncalomb.com>
2016-03-22 15:58:59 +0000

    Connect the 7 segment display

--- src/p3js-web/tab-io.js
+++ src/p3js-web/tab-io.js
@@ -1,11 +1,19 @@
 module.exports = function(p3sim) {
 
+	var $io_terminal = $("#io-terminal");
 	var $io_board_lcd = $("#io-board-lcd");
 	var $io_board_leds = $("#io-board-leds");
 	var $io_board_7seg = $("#io-board-7seg");
 	var $io_board_buttons = $("#io-board-buttons");
 	var $io_board_switches = $("#io-board-switches");
 	var $leds = [];
+
+	var seg7_value = 0;
+
+	function update_7seg(value) {
+		seg7_value = value;
+		$io_board_7seg.text(("0000" + seg7_value.toString(16)).substr(-4));
+	}
 
 	// populate board elements
 
@@ -37,5 +45,26 @@ module.exports = function(p3sim) {
 			}
 		}).appendTo($io_board_switches);
 	}
+
+	// register p3 simulator handlers
+
+	p3sim.registerEventHandler("reset", function() {
+		update_7seg(0);
+	});
+
+	p3sim.setIOHandler({
+		set7Seg0: function(v) {
+			update_7seg((seg7_value & 0xfff0) | (v & 0xf));
+		},
+		set7Seg1: function(v) {
+			update_7seg((seg7_value & 0xff0f) | ((v & 0xf) << 4));
+		},
+		set7Seg2: function(v) {
+			update_7seg((seg7_value & 0xf0ff) | ((v & 0xf) << 8));
+		},
+		set7Seg3: function(v) {
+			update_7seg((seg7_value & 0x0fff) | ((v & 0xf) << 12));
+		}
+	});
 
 };

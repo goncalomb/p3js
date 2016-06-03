34b91100cb002f5181ff752bfe3e6cb822a44c43
goncalomb <me@goncalomb.com>
2016-06-03 01:00:37 +0100

    Improve assembly editor, adds option to save files
    It's now possible to save and manage multiple assembly files on the
    browser. The files are stored locally.
    
    This also adds a new 'welcome.as' demo.

--- src/p3js-web/tab-assembler.js
+++ src/p3js-web/tab-assembler.js
@@ -2,7 +2,13 @@ module.exports = function(share, p3sim) {
 
 	var $document = $(document);
 
-	var $load_demo = $("#load-demo")
+	var $asm_editor_files = $("#asm-editor-files");
+	var $asm_editor_new = $("#asm-editor-new");
+	var $asm_editor_save = $("#asm-editor-save");
+	var $asm_editor_download = $("#asm-editor-download");
+	var $asm_editor_delete = $("#asm-editor-delete");
+	var $asm_editor_demos = $("#asm-editor-demos");
+
 	var $assemble = $("#assemble");
 	var $assemble_run = $("#assemble-run");
 	var $assemble_dl = $("#assemble-dl");
@@ -77,27 +83,159 @@ module.exports = function(share, p3sim) {
 		$code_mirror.css("height", "");
 	});
 
+	// saved files and demos
+	var dirty = false;
+	var saved_files = JSON.parse(localStorage.getItem("p3js-saved-files")) || {};
+	var current_file = null;
+	var current_file_is_demo = false;
+	var save_info = "The files are stored on your browser, they are not uploaded to a remote server.\nI recommend making a copy in case something unexpected happens.\n";
 	var demos = [
-		// "welcome.as",
+		"welcome.as",
 		"Demo1-clean.as"
 	];
-	demos.forEach(function(demo) {
-		$load_demo.append($("<option>").val(demo).text(demo));
-	});
-	function load_demo(demo) {
-		// Loading local files (file://) with jQuery may not work. Use:
-		// google-chrome --user-data-dir=$(mktemp -d) --incognito --allow-file-access-from-files --start-maximized
-		// Or use Firefox.
-		$.get("demos/" + demo, null, function(data) {
-			code_mirror.setValue(data);
-			code_mirror.clearHistory();
-		}, "text");
+
+	for (var key in saved_files) {
+		$asm_editor_files.append($("<option>").val(key).text(key));
 	}
-	$load_demo.change(function() {
-		var demo = $("option:selected", this).val();
-		load_demo(demo)
+	demos.forEach(function(demo) {
+		$asm_editor_demos.append($("<option>").val(demo).text(demo));
 	});
-	load_demo(demos[0]);
+
+	code_mirror.on("change", function() {
+		dirty = true;
+	});
+	$(window).on("beforeunload", function() {
+		if (dirty) {
+			return "It appears that you have unsaved changes.";
+		}
+	});
+
+	function editor_set(value) {
+		code_mirror.setValue(value);
+		code_mirror.clearHistory();
+		dirty = false;
+	}
+	function editor_confirm() {
+		return !dirty || confirm("It appears that you have unsaved changes. Continue?");
+	}
+	function editor_set_files_select(value) {
+		$asm_editor_files.val(value).attr("data-last", value);
+	}
+	function editor_set_demos_select(value) {
+		$asm_editor_demos.val(value).attr("data-last", value);
+	}
+	function editor_confirm_for_select($select) {
+		if (!editor_confirm()) {
+			$select.val($select.attr("data-last"));
+			return false;
+		}
+		$select.attr("data-last", $select.val());
+		return true;
+	}
+	function editor_clear() {
+		editor_set_files_select(null);
+		editor_set_demos_select(null);
+		editor_set("");
+		current_file = null;
+		current_file_is_demo = false;
+		$asm_editor_delete.attr("disabled", true);
+	}
+	function editor_load_file(name, is_demo) {
+		if (is_demo) {
+			editor_set_files_select(null);
+			editor_set("");
+			$.get("demos/" + name, null, function(data) {
+				editor_set(data);
+			}, "text");
+		} else {
+			editor_set_demos_select(null);
+			editor_set(saved_files[name]);
+		}
+		$asm_editor_delete.attr("disabled", is_demo);
+		current_file = name;
+		current_file_is_demo = is_demo;
+	}
+	function editor_save() {
+		if (!current_file || current_file_is_demo) {
+			var new_name = prompt(save_info + "\nFilename:");
+			while (true) {
+				if (new_name === null) {
+					return;
+				} else if (!new_name) {
+					new_name = prompt("Filename:");
+					continue;
+				}
+				if (new_name.substr(-3) != ".as") {
+					new_name += ".as";
+				}
+				if (saved_files[new_name] === undefined) {
+					break;
+				}
+				new_name = prompt("File exists, choose a different name. Filename:", new_name);
+			}
+			$asm_editor_files.append($("<option>").val(new_name).text(new_name));
+			editor_set_files_select(new_name);
+			editor_set_demos_select(null);
+			$asm_editor_delete.attr("disabled", false);
+			current_file = new_name;
+			current_file_is_demo = false;
+		}
+		saved_files[current_file] = code_mirror.getValue();
+		localStorage.setItem("p3js-saved-files", JSON.stringify(saved_files));
+		dirty = false;
+	}
+
+	code_mirror.addKeyMap({
+		"Ctrl-S": editor_save
+	});
+
+	$asm_editor_files.change(function() {
+		$this = $(this);
+		if (editor_confirm_for_select($this)) {
+			editor_load_file($this.val(), false);
+		}
+	});
+	$asm_editor_demos.change(function() {
+		$this = $(this);
+		if (editor_confirm_for_select($this)) {
+			editor_load_file($this.val(), true);
+		}
+	});
+
+	editor_set_files_select(null);
+	editor_set_demos_select("welcome.as");
+	$asm_editor_demos.change();
+
+	$asm_editor_new.click(function() {
+		if (editor_confirm()) {
+			editor_clear();
+			current_file = null;
+			current_file_is_demo = false;
+		}
+	});
+	$asm_editor_save.click(editor_save);
+	$asm_editor_download.click(function() {
+		var code = code_mirror.getValue();
+		if (!code) {
+			return;
+		} else if (current_file) {
+			share.downloadBuffer(code, current_file);
+		} else {
+			share.downloadBuffer(code, "code.as");
+		}
+	});
+	$asm_editor_delete.click(function() {
+		if (confirm("Are you sure you want to delete \"" + current_file + "\"?")) {
+			delete saved_files[current_file];
+			$asm_editor_files.children().filter(function() {
+				return $(this).val() == current_file;
+			}).remove();
+			localStorage.setItem("p3js-saved-files", JSON.stringify(saved_files));
+			editor_clear();
+		}
+	});
+
+	// assembler
 
 	var is_assembling = false;
 
---
+++ www/demos/welcome.as
@@ -0,0 +1,5 @@
+;
+; P3JS
+; Write some assembly code.
+; Then click 'Assemble and Run' to run it on the P3 simulator.
+;

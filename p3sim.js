c8c9ffb21b4b0979e37b772d5e42d7894c0036fd
Gonçalo Baltazar <me@goncalomb.com>
2017-01-21 19:49:32 +0000

    Add support for 7 segment display, timer and leds (offline simulator)

--- p3sim.js
+++ p3sim.js
@@ -8,7 +8,15 @@ if (!process.stdin.isTTY || !process.stdout.isTTY) {
 	process.stdout.write("no TTY");
 }
 
+var timer = new (require("./src/p3js-web/IOTimer.js"))(p3sim);
+
+p3sim.registerEventHandler("reset", function() {
+	timer.reset();
+});
+
 p3sim.setIOHandlers({
+	0xfff6: function() { return timer.getValue(); },
+	0xfff7: function() { return timer.state(); },
 	0xfff9: function() { return termui.getSwitches(); },
 	0xfffd: function() {
 		return (termui.peekLastKey() ? 1 : 0);
@@ -17,6 +25,13 @@ p3sim.setIOHandlers({
 		return termui.getLastKey();
 	}
 }, {
+	0xfff0: function(v) { termui.set7Seg(v & 0xf, 0xfff0); },
+	0xfff1: function(v) { termui.set7Seg((v & 0xf) << 4, 0xff0f); },
+	0xfff2: function(v) { termui.set7Seg((v & 0xf) << 8, 0xf0ff); },
+	0xfff3: function(v) { termui.set7Seg((v & 0xf) << 12, 0x0fff); },
+	0xfff6: function(v) { timer.setValue(v); },
+	0xfff7: function(v) { timer.control(v); },
+	0xfff8: function(v) { termui.setLeds(v); },
 	0xfffc: function(v) {
 		var x = v & 0xff;
 		var y = v >> 8 & 0xff;
--- src/termui.js
+++ src/termui.js
@@ -8,6 +8,8 @@ var term_cur_y = 0;
 
 var focus = 0;
 var switches_value = 0;
+var seg7_value = 0;
+var leds_value = 0;
 
 function hex_key_to_int(c) {
 	if (c >= 48 && c <= 57) {
@@ -36,6 +38,17 @@ function draw_inputs() {
 	if (focus == 0) { program.bg("blue"); }
 	program.write("TERM");
 	if (focus == 0) { program.bg("!blue"); }
+}
+
+function draw_7seg() {
+	program.move(28, 4);
+	program.write("7SEG: " + ("0000" + seg7_value.toString(16)).substr(-4));
+}
+
+function draw_leds() {
+	program.move(28, 3);
+	var leds_str = ("0000000000000000" + leds_value.toString(2)).substr(-16);
+	program.write("LEDS: " + leds_str.substr(0, 4) + " " + leds_str.substr(4, 4) + " " + leds_str.substr(8, 4) + " " + leds_str.substr(12));
 }
 
 function next_focus() {
@@ -73,6 +86,16 @@ function set_switches(c) {
 			program.write(((switches_value >> i) & 1).toString());
 		}, 100);
 	}
+}
+
+function set_7seg(v, mask) {
+	seg7_value = (seg7_value & mask) | v;
+	draw_7seg();
+}
+
+function set_leds(v) {
+	leds_value = v;
+	draw_leds();
 }
 
 termui.initialize = function(p3sim) {
@@ -163,10 +186,8 @@ termui.drawHeader = function() {
 	put_char(26, 3, 0x2502);
 	put_char(26, 4, 0x2502);
 	put_char(26, 5, 0x2534);
-	program.move(28, 3);
-	program.write("LEDS: 0000 0000 0000 0000");
-	program.move(28, 4);
-	program.write("7SEG: 0000");
+	draw_leds();
+	draw_7seg();
 	put_char(54, 2, 0x252c);
 	put_char(54, 3, 0x2502);
 	put_char(54, 4, 0x2502);
@@ -207,6 +228,9 @@ termui.getLastKey = function() {
 termui.getSwitches = function() {
 	return switches_value;
 }
+
+termui.set7Seg = set_7seg;
+termui.setLeds = set_leds;
 
 termui.dispose = function() {
 	process.removeListener("exit", termui.dispose);

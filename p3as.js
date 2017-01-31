c6c339855a6e7747c5c06680b3fb6a564c332bd5
Gonçalo Baltazar <me@goncalomb.com>
2017-01-31 22:44:44 +0000

    Add command-line options to the offline assembler (and code cleanup)

--- p3as.js
+++ p3as.js
@@ -3,28 +3,67 @@ var path = require("path");
 
 var p3js = require("./src/p3js/p3js");
 
-if (typeof process.argv[2] != "undefined") {
-	var input_file = process.argv[2];
-	if (input_file.slice(-3) != ".as") {
-		console.log("The input file must use the .as extension.");
-	} else {
-		fs.readFile(input_file, "utf8", function (err, data) {
-			if (err) {
-				return console.log(err);
-			}
-			var output_file = input_file.slice(0, -3) + ".exe";
-			console.log("Assembling '" + input_file + "'...");
-			try {
-				var result = p3js.assembly.assembleWithDefaultValidator(data);
-				var buffer = p3js.writeObjectFormat(result.buffer, false, result.usedAddresses);
-			} catch (e) {
-				console.log(e);
-			}
-			console.log("Writing to '" + output_file + "'...");
-			fs.writeFileSync(output_file, new Buffer(new Uint8Array(buffer)));
-			console.log("Done.");
-		});
+var argv = require("minimist")(process.argv.slice(2), {
+	string: ["o"],
+	boolean: ["show-refs"],
+	unknown: function(opt) {
+		if (opt[0] == '-') {
+			console.log("Unknown option '" + opt + "'.");
+			process.exit(1);
+			return false;
+		}
 	}
-} else {
+});
+
+if (typeof argv._[0] == "undefined") {
 	console.log("No input file.");
+	process.exit(2);
 }
+
+var input_file = path.normalize(argv._[0]);
+var path_parts = path.parse(input_file);
+var output_file = path.join(path_parts.dir, path_parts.name + ".exe");
+
+if (typeof argv.o == "string") {
+	output_file = path.normalize(argv.o);
+}
+
+try {
+	var data = fs.readFileSync(input_file, "utf8");
+} catch (e) {
+	console.error(e.message);
+	process.exit(3);
+}
+
+console.log("Assembling '" + input_file + "'...");
+try {
+	var result = p3js.assembly.assembleWithDefaultValidator(data);
+	var buffer = p3js.writeObjectFormat(result.buffer, false, result.usedAddresses);
+} catch (e) {
+	console.error("Assembly Error: " + e);
+	process.exit(4);
+}
+
+console.log("Statistics:");
+console.log("  References: " + result.labelCount);
+console.log("  Pseudo Instructions: " + result.pseudoCount);
+console.log("  Instructions: " + result.instructionCount);
+var memory_percent = Math.floor(result.memoryUsage*10000/p3js.devices.RAM.MEMORY_SIZE)/100;
+console.log("  Memory: " + result.memoryUsage + "/" + p3js.devices.RAM.MEMORY_SIZE + " (" + memory_percent + "%)");
+
+if (argv["show-refs"]) {
+	console.log("References:");
+	Object.keys(result.labels).forEach(function(name) {
+		console.log("  " + name + ": " +  ("0000" + result.labels[name].toString(16)).substr(-4));
+	});
+}
+
+console.log("Writing program to '" + output_file + "'...");
+try {
+	fs.writeFileSync(output_file, new Buffer(new Uint8Array(buffer)));
+} catch (e) {
+	console.error(e.message);
+	process.exit(5);
+}
+
+console.log("Done.");

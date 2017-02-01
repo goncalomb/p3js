32a7fbba1aa6ca97d876809b4e6af869cbf72231
Gonçalo Baltazar <me@goncalomb.com>
2017-02-01 15:25:04 +0000

    Cleanup offline simulator code (better error handling)

--- p3sim.js
+++ p3sim.js
@@ -1,20 +1,45 @@
 var fs = require("fs");
+var path = require("path");
+
 var termui = require("./src/termui.js");
 var p3js = require("./src/p3js/p3js.js");
+
+var argv = require("minimist")(process.argv.slice(2), {
+	unknown: function(opt) {
+		if (opt[0] == '-') {
+			console.error("Unknown option '" + opt + "'.");
+			process.exit(1);
+			return false;
+		}
+	}
+});
 
 var p3sim = new p3js.Simulator();
 
 if (!process.stdin.isTTY || !process.stdout.isTTY) {
-	process.stdout.write("no TTY");
+	console.error("Not connected to a tty.");
+	process.exit(2);
 }
 
-if (typeof process.argv[2] != "undefined") {
-	termui.initialize(p3sim);
-	fs.readFile(process.argv[2], "utf8", function (err, data) {
-		var result = p3js.assembly.assembleWithDefaultValidator(data);
-		p3sim.loadMemory(result.buffer);
-		p3sim.start();
-	});
-} else {
-	console.log("No input file.");
+if (typeof argv._[0] == "undefined") {
+	console.error("No input file.");
+	process.exit(3);
 }
+
+try {
+	var data = fs.readFileSync(path.normalize(argv._[0]), "utf8");
+} catch (e) {
+	console.error(e.message);
+	process.exit(4);
+}
+
+try {
+	var result = p3js.assembly.assembleWithDefaultValidator(data);
+} catch (e) {
+	console.error("Assembly Error: " + e);
+	process.exit(5);
+}
+
+termui.initialize(p3sim);
+p3sim.loadMemory(result.buffer);
+p3sim.start();

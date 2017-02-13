fcfaf3516ddcbaaacc5f874d9bde69203116d7f0
Gonçalo Baltazar <me@goncalomb.com>
2017-02-13 22:31:48 +0000

    Modularize the assembly editor (add new AssemblyEditor class)

--- src/p3js-web/tab-assembler.js
+++ src/p3js-web/tab-assembler.js
@@ -6,6 +6,7 @@
  */
 
 var p3js_web = require(".");
+var AssemblyEditor = require("../p3js-dom/AssemblyEditor.js");
 
 module.exports = function(p3sim) {
 
@@ -57,41 +58,13 @@ module.exports = function(p3sim) {
 	});
 
 	// editor
-	var $code = $("#code");
+	var editor = new AssemblyEditor(
+		$("#code-editor"),
+		$asm_editor_files,
+		$asm_editor_demos
+	);
 
-	var rulers = [];
-	for (var i = 0; i <= 80; i+= 8) {
-		rulers.push({ column: i, className: "asm-ruler-extra", color: "#dadada" });
-	}
-	rulers.push({ column: 16, className: "asm-ruler", color: "#c0c0ff" });
-	rulers.push({ column: 24, className: "asm-ruler", color: "#c0c0ff" });
-	rulers.push({ column: 80, className: "asm-ruler", color: "#ffc0c0" });
-
-	var code_mirror = CodeMirror.fromTextArea($code[0], {
-		lineNumbers: true,
-		indentUnit: 4,
-		gutters: ["CodeMirror-lint-markers"],
-		lint: true,
-		extraKeys: {
-			Tab: function(cm) {
-				var selections = cm.listSelections();
-				var strings = [];
-				for (var i = 0, l = selections.length; i < l; i++) {
-					var p = selections[i].from().ch;
-					if (p < 16) {
-						strings.push(Array(16 - p + 1).join(" "));
-					} else if (p < 24) {
-						strings.push(Array(24 - p + 1).join(" "));
-					} else {
-						strings.push(Array(cm.getOption("indentUnit") + 1).join(" "));
-					}
-				}
-				cm.replaceSelections(strings);
-			}
-		},
-		rulers: rulers
-	});
-	var $code_mirror = $(code_mirror.getWrapperElement());
+	var $code_mirror = $(editor.codeMirror.getWrapperElement());
 	$document.on("fullscreenon", function() {
 		$code_mirror.height($(window).height() - $code_mirror.offset().top - 20);
 	}).on("fullscreenoff", function() {
@@ -102,18 +75,20 @@ module.exports = function(p3sim) {
 				if ($(document.body).hasClass("fullscreen")) {
 					$code_mirror.height($(window).height() - $code_mirror.offset().top - 20);
 				}
-				code_mirror.refresh();
+				editor.codeMirror.refresh();
 			});
 		}
 	});
 
-	// saved files and demos
-	var dirty = false;
-	var saved_files = JSON.parse(localStorage.getItem("p3js-saved-files")) || {};
-	var current_file = null;
-	var current_file_is_demo = false;
-	var save_info = "The files are stored on your browser, they are not uploaded to a remote server.\nI recommend making a local copy (click the blue download button) in case something unexpected happens.\n";
-	var demos = [
+	$asm_editor_new.click(function() { editor.new(); });
+	$asm_editor_save.click(function() { editor.save(); });
+	$asm_editor_delete.click(function() { editor.delete(); });
+
+	editor.onFileChange(function(currentFile, currentFileIsDemo) {
+		$asm_editor_delete.attr("disabled", !(currentFile && !currentFileIsDemo));
+	});
+
+	editor.loadFiles([
 		"welcome.as",
 		{
 			label: "P3JS Demos",
@@ -127,163 +102,12 @@ module.exports = function(p3sim) {
 			label: "IST Demos",
 			files: [ "Demo1-clean.as" ]
 		}
-	];
+	]);
 
-	for (var key in saved_files) {
-		$asm_editor_files.append($("<option>").val(key).text(key));
-	}
-	demos.forEach(function(demo) {
-		if (typeof demo == "string") {
-			$asm_editor_demos.append($("<option>").val(demo).text(demo));
-		} else {
-			var $optgroup = $("<optgroup>").attr("label", demo.label);
-			demo.files.forEach(function(demo) {
-				$optgroup.append($("<option>").val(demo).text(demo));
-			});
-			$asm_editor_demos.append($optgroup);
-		}
-	});
-
-	code_mirror.on("change", function() {
-		dirty = true;
-	});
-	$(window).on("beforeunload", function() {
-		if (dirty) {
-			return "It appears that you have unsaved changes.";
-		}
-	});
-
-	function editor_set(value) {
-		code_mirror.setValue(value);
-		code_mirror.clearHistory();
-		dirty = false;
-	}
-	function editor_confirm() {
-		return !dirty || confirm("It appears that you have unsaved changes. Continue?");
-	}
-	function editor_set_files_select(value) {
-		$asm_editor_files.val(value).attr("data-last", value);
-	}
-	function editor_set_demos_select(value) {
-		$asm_editor_demos.val(value).attr("data-last", value);
-	}
-	function editor_confirm_for_select($select) {
-		if (!editor_confirm()) {
-			$select.val($select.attr("data-last"));
-			return false;
-		}
-		$select.attr("data-last", $select.val());
-		return true;
-	}
-	function editor_clear() {
-		editor_set_files_select(null);
-		editor_set_demos_select(null);
-		editor_set("");
-		current_file = null;
-		current_file_is_demo = false;
-		localStorage.removeItem("p3js-current-file");
-		$asm_editor_delete.attr("disabled", true);
-	}
-	function editor_load_file(name, is_demo) {
-		if (is_demo) {
-			editor_set_files_select(null);
-			editor_set("");
-			$.get("demos/" + name, null, function(data) {
-				editor_set(data);
-			}, "text");
-			localStorage.removeItem("p3js-current-file");
-		} else {
-			editor_set_demos_select(null);
-			editor_set(saved_files[name]);
-			localStorage.setItem("p3js-current-file", name);
-		}
-		$asm_editor_delete.attr("disabled", is_demo);
-		current_file = name;
-		current_file_is_demo = is_demo;
-	}
-	function editor_save() {
-		if (!current_file || current_file_is_demo) {
-			var new_name = prompt(save_info + "\nFilename:");
-			while (true) {
-				if (new_name === null) {
-					return;
-				} else if (!new_name) {
-					new_name = prompt("Filename:");
-					continue;
-				}
-				if (new_name.substr(-3) != ".as") {
-					new_name += ".as";
-				}
-				if (saved_files[new_name] === undefined) {
-					break;
-				}
-				new_name = prompt("File exists, choose a different name. Filename:", new_name);
-			}
-			$asm_editor_files.append($("<option>").val(new_name).text(new_name));
-			editor_set_files_select(new_name);
-			editor_set_demos_select(null);
-			$asm_editor_delete.attr("disabled", false);
-			current_file = new_name;
-			current_file_is_demo = false;
-			localStorage.setItem("p3js-current-file", current_file);
-		}
-		saved_files[current_file] = code_mirror.getValue();
-		localStorage.setItem("p3js-saved-files", JSON.stringify(saved_files));
-		dirty = false;
-	}
-
-	code_mirror.addKeyMap({
-		"Ctrl-S": editor_save
-	});
-
-	$asm_editor_files.change(function() {
-		$this = $(this);
-		if (editor_confirm_for_select($this)) {
-			editor_load_file($this.val(), false);
-		}
-	});
-	$asm_editor_demos.change(function() {
-		$this = $(this);
-		if (editor_confirm_for_select($this)) {
-			editor_load_file($this.val(), true);
-		}
-	});
-
-	current_file = localStorage.getItem("p3js-current-file");
-	if (current_file) {
-		editor_set_files_select(current_file);
-		editor_set_demos_select(null);
-		$asm_editor_files.change();
-	} else {
-		editor_set_files_select(null);
-		editor_set_demos_select("welcome.as");
-		$asm_editor_demos.change();
-	}
-
-	$asm_editor_new.click(function() {
-		if (editor_confirm()) {
-			editor_clear();
-		}
-	});
-	$asm_editor_save.click(editor_save);
 	$asm_editor_download.click(function() {
-		var code = code_mirror.getValue();
-		if (!code) {
-			return;
-		} else if (current_file) {
-			p3js_web.downloadBuffer(code, current_file);
-		} else {
-			p3js_web.downloadBuffer(code, "code.as");
-		}
-	});
-	$asm_editor_delete.click(function() {
-		if (confirm("Are you sure you want to delete \"" + current_file + "\"?")) {
-			delete saved_files[current_file];
-			$asm_editor_files.children().filter(function() {
-				return $(this).val() == current_file;
-			}).remove();
-			localStorage.setItem("p3js-saved-files", JSON.stringify(saved_files));
-			editor_clear();
+		var code = editor.getValue();
+		if (code) {
+			p3js_web.downloadBuffer(code, editor.getFileName());
 		}
 	});
 
@@ -299,7 +123,7 @@ module.exports = function(p3sim) {
 			setTimeout(function() {
 				var t = Date.now();
 				try {
-					var result = p3js.assembly.assembleWithDefaultValidator(code_mirror.getValue());
+					var result = p3js.assembly.assembleWithDefaultValidator(editor.getValue());
 					asm_info_add(null)
 					asm_info_add("Assembling finished (" + (Date.now() - t) + " ms).", "text-success");
 					asm_info_add("Program loaded on simulator.", "text-info small");
@@ -337,16 +161,7 @@ module.exports = function(p3sim) {
 
 	$assemble_dl.click(function() {
 		assemble_program(function(result) {
-			var name = "code.exe";
-			if (current_file) {
-				var i = current_file.lastIndexOf(".");
-				if (i != -1) {
-					name = current_file.substr(0, i) + ".exe";
-				} else {
-					name = current_file + ".exe";
-				}
-			}
-			p3js_web.downloadBuffer(result.buildProgramCode(), name);
+			p3js_web.downloadBuffer(result.buildProgramCode(), editor.getFileName("exe"));
 			asm_info_add("Download requested (p3as format).", "text-info small");
 		});
 	});
